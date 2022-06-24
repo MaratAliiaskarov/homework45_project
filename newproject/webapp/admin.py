f0d0738391f461b7329ac1c1a175403f70fe5376
apple </Kvadrat2020/>
2022-06-24 00:08:47 -0400

    created file admin and settings

--- newproject/webapp/admin.py
+++ newproject/webapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'project', 'author', 'created_at']
+    list_display_links = ['project']
+    list_filter = ['author']
+    search_fields = ['project', 'content']
+    fields = ['project', 'author', 'content', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Article, ArticleAdmin)
