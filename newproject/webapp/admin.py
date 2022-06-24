84d8b00d5c33f0310e0d71bdc04148fd13628fb0
apple </Kvadrat2020/>
2022-06-24 08:58:11 -0400

    update baza dannyh

--- newproject/webapp/admin.py
+++ newproject/webapp/admin.py
@@ -5,7 +5,7 @@ from webapp.models import Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id', 'project', 'created_at']
+    list_display = ['id', 'project']
     list_display_links = ['id', 'project']
     list_filter = ['project']
     search_fields = ['project', 'status']
